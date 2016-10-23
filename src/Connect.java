0a293cc5dcb7ed099ceaddcbea3c45959c1fce3d
denisoetken <denisoetken@hotmail.de>
2016-10-23 20:48:42 +0200

    First

---
+++ src/Connect.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ * Created by Denis on 23.10.2016.
+ */
+public class Connect {
+    private static String url = "";
+    private static String user = "";
+    private static String pass = "";
+    private static Connection con;
+
+    public static void main(String[] args) {
+        konfigEinlesen();
+        try {
+            verbindenDB();
+            disconnect();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void verbindenDB() throws SQLException {
+        con = DriverManager.getConnection(url, user, pass);
+    }
+
+    private static void konfigEinlesen() {
+        Properties prop = new Properties();
+        try {
+            BufferedReader bfr = new BufferedReader(new FileReader("src/konfig.properties"));
+            prop.load(bfr);
+            bfr.close();
+            url = prop.getProperty("db_url");
+            user = prop.getProperty("db_user");
+            pass = prop.getProperty("db_pass");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void disconnect() throws SQLException {
+        con.close();
+    }
+}
